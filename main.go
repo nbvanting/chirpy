af435267d1753118e8e07f93202e17a306c23521
nbvanting <nicolai@vanting.com>
2025-03-03 13:22:06 +0100

    fileservers

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
 
+	// Serve static files from the current directory
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileServer)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
